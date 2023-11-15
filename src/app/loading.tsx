626665b991996fcb94bab5a2b6007e287741248d
NishantGupt786 <nishantgupta2325@gmail.com>
2023-11-16 02:31:51 +0530

    final changes

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import SkeletonCard from "@/components/SkeletonCard";
 export default function Loading() {
   return (
     <main>
-      <div className="grid grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 lg:grid-cols-3 2xl:grid-cols-5 gap-8">
         {"abcdefghi".split('').map(i => (
           <SkeletonCard key={i} />
         ))}
