4950cca120b1ac324ba2dbf03c71c90ad6f01230
NishantGupt786 <nishantgupta2325@gmail.com>
2023-11-16 02:59:11 +0530

    added toggle theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/Theme-Provider";
+import { ModeToggle } from "@/components/Toggle-Theme";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Shadecn Experiment",
-  description: "This is an experiment website made using shadecn as a ui library.",
+  description:
+    "This is an experiment website made using shadecn as a ui library.",
 };
 
 export default function RootLayout({
@@ -14,13 +17,21 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav className="mx-8 md:mx-auto">
-          <h1>Navbar</h1>
-        </nav>
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <nav className="mx-8 md:mx-auto flex justify-between">
+            <h1>Navbar</h1><ModeToggle />
+          </nav>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
